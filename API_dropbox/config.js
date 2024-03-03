6221da816035a85359d3b5320145cfcc706ab43b
Daniela Zapata <daniela_zapata@epam.com>
2024-03-03 16:09:46 -0500

    added tests for Dropbox API with Axios and chai

---
+++ API_dropbox/config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    DROPBOX_ACCESS_TOKEN: 'sl.BwuWNN9g2U3tAY0yXPvSbRUXuMIhBDkPQYzKYeIurBA5JoFk3CjMuyGTrBmJ9ZtL2bLQnZklW8RRgubWRHlVlf25knNi20EuQga6PKTxhP8QUrfKUangIj317vQU8EHKK0TcmcIechZF_KmwBNjoTrc',
+    DROPBOX_API_URL: 'https://api.dropboxapi.com/2',
+    DROPBOX_CONTENT_URL: 'https://content.dropboxapi.com/2'
+};
