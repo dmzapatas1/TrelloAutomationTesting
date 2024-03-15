8e29fe6dc375bd23c4b37761ad232574f96117ce
Daniela Zapata <daniela_zapata@epam.com>
2024-03-15 12:27:06 -0500

    updated token 240315 12:26 pm

--- API_dropbox/config.js
+++ API_dropbox/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DROPBOX_ACCESS_TOKEN:
-    'sl.BxepznFPckMz_uTgymMZ7jyUIk7zH9Y2syISvtonJu_A5P6DIe-crTj7_V5-fKzcCet5IbhM1A3YU2ciUsER8j3kELKnG5XqEU2EWvf6t18ZU8lnz1WRIfX1rj55hscdG2fABzdbVVkNSWjNlTqtUW8',
+    'sl.BxfB2132vvThFBShD-BpblwWXg96CLD55lIUC4WpVe4qDTA1Q5jLtHcoXZJt7jN8fQ8C8HEVz6Hudocj5ih0Pq1LakZP-VLBq7Nz49WeEwgcMjqPnJd7lkX6kpv1CUU4s9PtcjCqhuIxIqnkkJPXtxk',
   DROPBOX_API_URL: 'https://api.dropboxapi.com/2',
   DROPBOX_CONTENT_URL: 'https://content.dropboxapi.com/2'
 };
